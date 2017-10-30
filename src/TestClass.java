8ec03307a9a650eeb6a72ffa69e29388d39c38f3
YermRodion <yermolinrodion@gmail.com>
2017-10-30 22:00:08 +0600

    Task1

---
+++ src/TestClass.java
@@ -0,0 +1,13 @@
+/**
+ * Created by RODION on 30.10.2017.
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        TestClass testClass = new TestClass();
+        testClass.printTestText();
+    }
+
+    public void printTestText(){
+        System.out.println("Success!");
+    }
+}
